64c896baffc6c2b54afbc8e803ae9fc63791cc42
Чернышов Марк home <mark.chernyshov.2003@mail.ru>
2024-09-08 23:56:15 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -6,5 +6,12 @@ function showTime() {
         
     // Обновляем секунды в центре страницы
     document.getElementById('seconds').innerHTML = currentSeconds < 10 ? '0' + currentSeconds : currentSeconds;
+    
+    const seconds = today.getSeconds();
+    const secondsDisplay = document.getElementById('seconds');
+    const secondHand = document.getElementById('second-hand');
+    // Расчёт вращения стрелки
+    const secondsDegree = ((seconds / 60) * 360) + 90; // Смещение на 90 градусов для начала с 12:00
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 }
 setInterval(showTime, 1000);
